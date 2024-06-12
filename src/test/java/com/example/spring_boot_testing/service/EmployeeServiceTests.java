116e189d3330295f43db7ca55b46a4bdeeec4f5a
oguzkaankaradag <karadagoguzkaan@gmail.com>
2024-06-12 19:58:06 +0300

    JUnit test for saveEmployee method

---
+++ src/test/java/com/example/spring_boot_testing/service/EmployeeServiceTests.java
@@ -0,0 +1,65 @@
+package com.example.spring_boot_testing.service;
+
+import com.example.spring_boot_testing.model.Employee;
+import com.example.spring_boot_testing.repository.EmployeeRepository;
+import com.example.spring_boot_testing.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTests {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    private Employee employee;
+
+    @BeforeEach
+    public void setup(){
+        //employeeRepository = Mockito.mock(EmployeeRepository.class);
+        //employeeService = new EmployeeServiceImpl(employeeRepository);
+        employee = Employee.builder()
+                .id(1L)
+                .firstname("Oguz")
+                .lastname("KARADAG")
+                .email("karadagoguzkaan@gmail.com")
+                .build();
+    }
+
+    // JUnit test for saveEmployee method
+    @DisplayName("JUnit test for saveEmployee method")
+    @Test
+    public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject(){
+        // given - precondition or setup
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.empty());
+
+        given(employeeRepository.save(employee)).willReturn(employee);
+
+        System.out.println(employeeRepository);
+        System.out.println(employeeService);
+
+        // when -  action or the behaviour that we are going test
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+
+        System.out.println(savedEmployee);
+        // then - verify the output
+        assertThat(savedEmployee).isNotNull();
+    }
+
+
+
+}
