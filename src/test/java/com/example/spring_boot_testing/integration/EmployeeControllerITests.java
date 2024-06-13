68f56912a1225a4d99d8167e0e0471d0cd0dc4eb
oguzkaankaradag <karadagoguzkaan@gmail.com>
2024-06-13 11:09:52 +0300

    JUnit I test for GET employee by id REST API

--- src/test/java/com/example/spring_boot_testing/integration/EmployeeControllerITests.java
+++ src/test/java/com/example/spring_boot_testing/integration/EmployeeControllerITests.java
@@ -13,9 +13,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -64,6 +69,49 @@ public class EmployeeControllerITests {
                         is(employee.getLastname()))) // Adjusted field name
                 .andExpect(jsonPath("$.email", // Corrected JSON path
                         is(employee.getEmail()))); // Adjusted field name
+    }
+
+    @Test
+    @DisplayName("JUnit I test for Get All employees REST API")
+    public void givenListOfEmployees_whenGetAllEmployees_thenReturnEmployeesList() throws Exception{
+        // given - precondition or setup
+        List<Employee> listOfEmployees = new ArrayList<>();
+        listOfEmployees.add(Employee.builder().firstname("Ramesh").lastname("Fadatare").email("ramesh@gmail.com").build());
+        listOfEmployees.add(Employee.builder().firstname("Tony").lastname("Stark").email("tony@gmail.com").build());
+        employeeRepository.saveAll(listOfEmployees);
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/api/employees"));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()",
+                        is(listOfEmployees.size())));
+
+    }
+
+    // positive scenario - valid employee id
+    @Test
+    @DisplayName("JUnit test for GET employee by id REST API")
+    public void givenEmployeeId_whenGetEmployeeById_thenReturnEmployeeObject() throws Exception{
+        // given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstname("Oguz")
+                .lastname("KARADAG")
+                .email("karadagoguzkaan@gmail.com")
+                .build();
+        employeeRepository.save(employee);
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/api/employees/{id}", employee.getId()));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstname", is(employee.getFirstname())))
+                .andExpect(jsonPath("$.lastname", is(employee.getLastname())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
     }
 
 }
